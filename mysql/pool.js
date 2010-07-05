7995aa8d9232422c49e83afc37b3783360ba4915
Andrey Sidorov <sidorares@yandex.ru>
2010-07-06 02:11:46 +0400

    minor fixeds

--- mysql/pool.js
+++ mysql/pool.js
@@ -27,20 +27,26 @@ pool.prototype.spawnConnection = function()
     var client = this.newConnectionFactory();
     var self = this;
     // todo: Qt-style connection-slot api?
-    client.connection.addListener('command_end', function() { self.queueChanged(client); });
+    client.connection.addListener('queue', function(new_size) { self.queueChanged(client, new_size); });
     var node = this.connections.push(client);
     client.pool_node = node;
     return client;
 }
 
-pool.prototype.queueChanged = function(client)
+pool.prototype.queueChanged = function(client, new_size)
 {
-    var new_size = client.commands.length;
+    if (new_size != 0)
+        return;
+
+
+    sys.puts('queue event ' + new_size);
+    //var new_size = client.commands.length;
+
     // if (new_size == 1)
     //    sys.p(client.commands.begin.data);
     // sys.puts("New queue:" + new_size);
 
-    if (!this.waiters.empty() && new_size <= this.maxQueue)
+    if (!this.waiters.empty() && new_size == 0) //<= this.maxQueue)
     {
         var w = this.waiters.shift();
         sys.puts("free connection released to waiter" );
@@ -48,7 +54,7 @@ pool.prototype.queueChanged = function(client)
             w(client);
     }
 
-    // there is no c:0ommands left for current connection
+    // there is no commands left for current connection
     // close it after idleTimeout
     if (new_size == 0 && this.connections.length > this.minConnections)
     {
