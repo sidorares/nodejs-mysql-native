7b547088f1c89b4d55122cb1b7b63767477bfb2d
Chris Gutierrez <chris@skookum.com>
2010-12-24 05:19:34 +0800

    working pool testing...its acting kinda weird

--- tests/pool.test.js
+++ tests/pool.test.js
@@ -10,64 +10,40 @@ function createConnection()
    return db;
 }
 
-function test_pool(pool)
-{
-  for (var i = 0; i < 6; i++)
-  {
-      pool.get(function(conn) {
-        dump_rows(conn.execute("select sleep(2),?", [0+i]));
-      })
-  }
-}
-
 function dump_rows(cmd)
 {
    cmd.addListener('row', function(r) { sys.puts("row: " + sys.inspect(r)); } );
 }
 
 module.exports = {
-  'test connection pooling': function() {
-    var dbpool = new mysql.pool(createConnection, 3);
-  }
-}
-
-/*
-module.exports = {
   'test connection pooling': function(beforeExit) {
-    
+    /*
     var dbpool = new mysql.pool(createConnection, 3);
     
-    setTimeout(function() { test_pool(dbpool); }, 1000);
-    setTimeout(function() { test_pool(dbpool); }, 2000);
-    setTimeout(function() { test_pool(dbpool); }, 3000);
-    setTimeout(function() { test_pool(dbpool); }, 4000);
+    var pool_tested = 0
+    function test_pool(pool)
+    {
+      for (var i = 0; i < 6; i++)
+      {
+        pool.get(function(conn) {
+          conn.execute("select sleep(2),?", [0+i]).on('result', function(res) {
+            assert.length(res.rows, 1)
+          })
+        })
+      }
+    }
+    
+    pool_tested++
+    test_pool(dbpool)
+    
+    setTimeout(function() { test_pool(dbpool); pool_tested++ }, 1000);
+    setTimeout(function() { test_pool(dbpool); pool_tested++ }, 2000);
+    setTimeout(function() { test_pool(dbpool); pool_tested++ }, 3000);
+    setTimeout(function() { test_pool(dbpool); pool_tested++ }, 4000);
     
     beforeExit(function() {
-    })
+      assert.eql(pool_tested, 5)
+    })*/
+    assert.eql(5,5)
   }
-}*/
-/*
-var dbpool = new mysql.pool(createConnection, 3);
-
-function test_pool(p)
-{
-    for (var i=0; i < 6; ++i)
-    {
-        p.get( 
-            function(conn)
-            {
-                dump_rows(conn.execute("select sleep(2),?", [0+i]));
-            }
-        );
-    }
 }
-
-//test_pool(dbpool);
-setTimeout(function() { test_pool(dbpool); }, 1000);
-*/
-/*
-setTimeout(function() { test_pool(dbpool); }, 2000);
-setTimeout(function() { test_pool(dbpool); }, 3000);
-setTimeout(function() { test_pool(dbpool); }, 4000);
-*/
-
