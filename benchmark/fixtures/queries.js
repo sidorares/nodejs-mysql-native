51c055afdc10649db6a53aeebbf1ddca0582a844
Andrey Sidorov <laplace@Ubuntu-1010-maverick-64-minimal.(none)>
2011-05-13 02:50:25 +0200

    dont error if db exists

--- benchmark/fixtures/queries.js
+++ benchmark/fixtures/queries.js
@@ -2,15 +2,16 @@ var conf = require('./config');
 
 // TODO: add code to read all *.sql files to help author large queries
 module.exports = {
-    createdb: 'create database ' + conf.db,
+    createdb: 'create database if not exists ' + conf.db,
     use: 'use ' + conf.db,
     drop: 'DROP TABLE IF EXISTS '+ conf.table,
-    create_table:   'CREATE TABLE ' + conf.table + ' ('+
+    create_table:   'CREATE TABLE IF NOT EXISTS ' + conf.table + ' ('+
       'id INT(11) AUTO_INCREMENT, '+
       'title VARCHAR(255), '+
       'text TEXT, '+
       'created DATETIME, '+
       'PRIMARY KEY (id));',
     insert: 'INSERT INTO ' + conf.table + ' SET title = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"',
-    insert_param: 'INSERT INTO ' + conf.table + ' SET title = "?"'
+    insert_param: 'INSERT INTO ' + conf.table + ' SET title = "?"',
+    select: 'select * from ' + conf.table + ' limit 100'
 }
