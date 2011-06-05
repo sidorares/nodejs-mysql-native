ce344e00fb4e82ac192a842b809fba50fcfe8beb
Andrey Sidorov <sidorares@yandex.ru>
2011-06-05 22:55:56 +0000

    BIGINT and BITSET test by @Quantivo

---
+++ tests/bigint-bf.test.js
@@ -0,0 +1,74 @@
+var assert = require('assert'), 
+mysql = require('../lib/mysql-native');
+
+function createConnection()     
+{
+    var db = mysql.createTCPClient(); 
+    //db.verbose = true;
+    db.set('auto_prepare', true)
+      .set('row_as_hash', false)
+      .auth("test", "testuser", "testpass");
+    return db;
+}
+
+function runTest(params, setupSql, insertSql, insertParams, testSql, testParams, rowTest){
+    var db = createConnection();
+    console.log(runTest.arguments);
+    db.query(setupSql).on("end", function(){
+        db.execute(insertSql, insertParams).on("end", function(){
+            db.execute(testSql, testParams)
+            .on("row", function(r){
+                console.log('row');
+                console.log(r);
+                rowTest(r);
+            })
+            .on('field', function(f) {
+                console.log(f);
+            })
+            .on("end", function(){
+                db.close();
+            });
+        });
+    });
+}
+
+exports = {
+    'test large integers': function(params) {
+        var setupSql = 'CREATE TEMPORARY TABLE test_int (\
+            id bigint(11) unsigned NOT NULL AUTO_INCREMENT, \
+            a bigint(11) unsigned NULL, \
+            PRIMARY KEY (id) \
+            )';
+        var insertSql = 'INSERT INTO test_int (a) VALUES (?)';
+        var testSql = 'SELECT a FROM test_int';
+
+        var testInt = 13109526528;
+        console.log('testInt = ' + testInt);
+
+        runTest(params, setupSql, insertSql, [testInt], testSql, [], function(r){
+            assert.equal(r[0],testInt);
+        });    
+     },
+
+    'test bit fields': function(params) {
+        var setupSql = 'CREATE TEMPORARY TABLE test_bf (\
+            id int(11) unsigned NOT NULL AUTO_INCREMENT, \
+            a BIT(4) NULL, \
+            PRIMARY KEY (id) \
+            )';
+        var insertSql = "INSERT INTO test_bf (a) VALUES (?)";
+        var testSql = 'SELECT a FROM test_bf';
+
+        var bitField = [false, true];
+        runTest(params, setupSql, insertSql, [bitField], testSql, [], function(r){
+            if(r[0] instanceof Array){
+                assert.deepEqual(r[0], bitField);
+            } else {
+                assert.equal(r[0],bitField);
+            }
+        });
+    }
+}
+
+//exports['test large integers']();
+//exports['test bit fields']();
