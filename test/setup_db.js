cc2cf61c074836655cbb8043a8011a4115bb9134
sidorares <sidorares@yandex.ru>
2011-11-17 16:19:23 +1100

    pre-test script midified for new API

--- test/setup_db.js
+++ test/setup_db.js
@@ -1,8 +1,7 @@
 var assert = require('assert')
 var mysql = require('../lib/mysql-native')
 
-var db = mysql.createTCPClient();
-db.auth();
+var db = mysql.createClient();
 db.query('create database test').on('end', function() {
     db.close();
 });
