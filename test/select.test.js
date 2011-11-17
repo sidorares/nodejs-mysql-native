0ab8e51c2e38e2f378b5f6affa6536154b0e3d5e
sidorares <sidorares@yandex.ru>
2011-11-17 18:03:21 +1100

    try to create user in precreate script

--- test/select.test.js
+++ test/select.test.js
@@ -5,7 +5,7 @@ module.exports = {
 
   'test select 1+1 + callback': function(cb) {
     //var db = mysql.createClient({ database: 'test', user: '', password: '', port: 3306, host: '127.0.0.1' });
-    var db = mysql.createClient({ database: 'test', socket: '/var/run/mysqld/mysqld.sock' });
+    var db = mysql.createClient({ database: 'test', user: 'tester', password: 'testpass', socket: '/var/run/mysqld/mysqld.sock' });
     db.query('select 1+1 as qqq', function(err, res) {
       console.log([err, res])
       this.connection.socket.end();
