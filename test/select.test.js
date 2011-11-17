18c7d349836d2d30572f1a8022d1b1fbb245c99e
sidorares <sidorares@yandex.ru>
2011-11-17 16:17:19 +1100

    start tests rewrite for new API

--- test/select.test.js
+++ test/select.test.js
@@ -1,86 +1,15 @@
-var sys = require('sys')
-  , assert = require('assert')
-  , mysql = require('../lib/mysql-native')
+var mysql = require('../lib/mysql-native')
 
 module.exports = {
-  'test select all rows row event': function() {
 
-    var db = mysql.createTCPClient()
-    db.set('auto_prepare', true)
-      .set('row_as_hash', true)
-      .auth("test", "testuser", "testpass")
-
-    var sql = 'SELECT * FROM tbl'
-    db.query(sql).addListener('row', function(r) {
-      assert.ok(typeof r == 'object')
-    }).addListener('end', function() {
-      db.close()
-    })
-    
+  'test select 1+1 + callback': function(a, b, c) {
+    console.log(a,b,c);
+    var db = mysql.createClient({ database: 'test' });
+    db.query('select 1+1 as qqq', function(err, res) {
+      db.close();
+    });  
   },
   
   'test select all rows result event': function() {
-    var db = mysql.createTCPClient()
-    db.set('auto_prepare', true)
-      .auth("test", "testuser", "testpass")
-
-    var sql = 'SELECT * FROM tbl'
-    db.query(sql).addListener('result', function(r) {
-      assert.ok(r.rows.length > 0)
-    }).addListener('end', function() {
-      db.close()
-    })
   },
-  
-  'test select no results': function() {
-    var db = mysql.createTCPClient()
-    db.set('auto_prepare', true)
-      .auth("test", "testuser", "testpass")
-
-    var sql = 'SELECT * FROM tbl WHERE id = 1000000000'
-    db.query(sql).addListener('resulti', function(r) {
-      assert.ok(r.rows.length == 0)
-    }).addListener('end', function() {
-      db.close()
-    })
-  },
-  
-  'test result field null number': function() {
-    var db = mysql.createTCPClient()
-    db.set('auto_prepare', true)
-      .set('row_as_hash', true)
-      .auth("test", "testuser", "testpass")
-
-    // first insert a record that we know has a null value
-    db.query('INSERT INTO tbl SET parent = NULL').addListener('result', function(res) {
-      var insert_id = res.insert_id
-      
-      var sql = 'SELECT * FROM tbl WHERE id = ' + insert_id
-      db.query(sql).addListener('row', function(r) {
-        assert.isNull(r.parent)
-      })
-    }).addListener('end', function() {
-      db.close()
-    })
-  },
-  
-  'test result field non null number': function() {
-    var db = mysql.createTCPClient()
-    db.set('auto_prepare', true)
-      .set('row_as_hash', true)
-      .auth("test", "testuser", "testpass")
-
-    // first insert a record that we know has a null value
-    db.query('INSERT INTO tbl SET parent = 1').addListener('end', function() {
-      var insert_id = this.result.insert_id
-
-      var sql = 'SELECT * FROM tbl WHERE id = ' + insert_id
-      db.query(sql).addListener('row', function(r) {
-        assert.eql(r.parent, 1)
-      })
-      
-    }).addListener('end', function() {
-      db.close()
-    })
-  }
 }
