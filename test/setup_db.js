1fc714119f198930e577d258e647d8ece2b1d02d
sidorares <sidorares@yandex.ru>
2011-11-18 11:34:13 +1100

    temporarily remove setup_db code

--- test/setup_db.js
+++ test/setup_db.js
@@ -1,7 +1,9 @@
 var mysql = require('../lib/mysql-native')
 var createConnection = require('./common').createConnection;
 
+/*
 var db = createConnection();
 db.query('create database if not exists test', function() {
     db.close();
 });
+*/
